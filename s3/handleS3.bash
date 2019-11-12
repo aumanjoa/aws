5d7bdba5469223cf7c44645d9376721fcd0a9f65
Joachim Aumann <aumannjoachim@gmail.com>
2019-11-12 21:45:33 +0100

    s3 implementation

---
+++ s3/handleS3.bash
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+aws s3 mb s3://joachim-hello-aws
+
+aws s3 cp index.html s3://joachim-hello-aws
+
+aws s3api put-object-acl --bucket joachim-hello-aws --key index.html --acl public-read
+
+#https://joachim-hello-aws.s3.eu-central-1.amazonaws.com/index.html
+
+aws cloudfront create-distribution \
+  --origin-domain-name joachim-hello-aws.s3.eu-central-1.amazonaws.com \
+  --default-root-object index.html
