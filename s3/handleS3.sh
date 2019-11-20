09da58287dec3c4d83d4e1aa9c28a0708e660915
Joachim Aumann <aumannjoachim@gmail.com>
2019-11-20 21:20:34 +0100

    Update handleS3.sh

--- s3/handleS3.sh
+++ s3/handleS3.sh
@@ -6,10 +6,6 @@ aws s3 cp index.html s3://joachim-hello-aws
 
 aws s3api put-object-acl --bucket joachim-hello-aws --key index.html --acl public-read
 
-#https://joachim-hello-aws.s3.eu-central-1.amazonaws.com/index.html
-
 aws cloudfront create-distribution \
   --origin-domain-name joachim-hello-aws.s3.eu-central-1.amazonaws.com \
   --default-root-object index.html
-
-#https://d2q47f3428j8u0.cloudfront.net/
