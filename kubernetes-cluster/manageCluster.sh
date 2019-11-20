9ddf937cc921afc759468ad5c6537a7919f88313
Joachim Aumann <aumannjoachim@gmail.com>
2019-11-20 20:54:36 +0100

    finished kubernetes aws example

--- kubernetes-cluster/manageCluster.sh
+++ kubernetes-cluster/manageCluster.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
 
-eksctl create cluster \
---name k8s \
---version 1.14 \
---region us-west-2 \
---nodegroup-name standard-workers \
---node-type t2.micro \
---nodes 2 \
---nodes-min 1 \
---nodes-max 3 \
---node-ami auto
+eksctl create cluster \                                                                                               ✔  3173  21:41:23
+> --name k8s \
+> --version 1.14 \
+> --region eu-central-1 \
+> --nodegroup-name standard-workers \
+> --node-type t2.micro \
+> --nodes 2 \
+> --nodes-min 1 \
+> --nodes-max 2 \
+> --node-ami auto
+
+
+kubectl apply -f rbac.yaml
+
+helm init --service-account tiller
+
+helm repo update
+
+helm repo add bitnami https://charts.bitnami.com/bitnami
+
+helm install --name my-release stable/nginx-ingress
+
+eksctl scale nodegroup --cluster=k8s --nodes=4 --name=standard-workers
